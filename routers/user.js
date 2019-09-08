0ac6669faeb30ee9519b79edf949cb1281511ad7
cmmanik <mkmanik6523u@gmail.com>
2019-09-08 17:13:09 +0600

    add material ui and creae registraion form is create

--- routers/user.js
+++ routers/user.js
@@ -3,16 +3,18 @@ const regiester = require('../validation/register');
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+
+// register a new user
 router.post('/regiester', (req, res) => {
     const validate = regiester(req.body);
     if (!validate.isValid) {
         res.status(400).json(validate.errrors)
     } else {
-        let {firstName, lastName, email, password} = req.body;
-        User.findOne({email})
+        let { firstName, lastName, email, password } = req.body;
+        User.findOne({ email })
             .then(user => {
-                if(user) {
-                    return res.status(400).json({meassage:'Email Already Exist!'})
+                if (user) {
+                    return res.status(400).json({ meassage: 'Email Already Exist!' })
                 }
 
                 let newUser = new User({
@@ -23,7 +25,7 @@ router.post('/regiester', (req, res) => {
                 })
 
                 bcrypt.genSalt(10, (err, salt) => {
-                    if(!err) {
+                    if (!err) {
                         bcrypt.hash(newUser.password, salt, (err, hash) => {
                             newUser.password = hash;
                             newUser.save()
@@ -31,15 +33,15 @@ router.post('/regiester', (req, res) => {
                                     res.status(200).json(result)
                                 })
                                 .catch(err => {
-                                    res.json({meassage:'Please try again later'})
+                                    res.json({ meassage: 'Please try again later' })
                                 })
                         })
                     } else {
-                        res.json({meassage:'Please Try again later!'})
+                        res.json({ meassage: 'Please Try again later!' })
                     }
                 })
             }).catch((err) => {
-                res.json({meassage:'Please try again'})
+                res.json({ meassage: 'Please try again' })
             });
     }
 
@@ -49,29 +51,29 @@ router.post('/regiester', (req, res) => {
 
 // login User
 router.post('/login', (req, res) => {
-    const {email, password} = req.body;
-    User.findOne({email})
+    const { email, password } = req.body;
+    User.findOne({ email })
         .then(user => {
-            if(!user) {
-                return res.json({meassage:'This email not registerd!'})
+            if (!user) {
+                return res.json({ meassage: 'This email not registerd!' })
             }
             bcrypt.compare(password, user.password, (err, result) => {
-                if(!err) {
-                    if(result) {
+                if (!err) {
+                    if (result) {
                         const payload = {
-                            _id:user._id,
-                            email:user.email
+                            _id: user._id,
+                            email: user.email
                         }
-                      let token = jwt.sign(payload, 'CMMANIK', {expiresIn:'2h'})
-                        return res.status(200).json({token:`Bearer ${token}`, msg:'Successuly Login'})
+                        let token = jwt.sign(payload, 'CMMANIK', { expiresIn: '2h' })
+                        return res.status(200).json({ token: `Bearer ${token}`, msg: 'Successuly Login' })
                     }
                     else {
-                       return res.json({msg:'Password does not match'})
+                        return res.json({ msg: 'Password does not match' })
                     }
                 }
             })
         })
-        .catch(err => res.json(err) )
+        .catch(err => res.json(err))
 })
 
 
